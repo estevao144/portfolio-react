c7c42dd2141a1ae06b7359874a5cbf3a2dd14ad4
Estevao <estevo144@gmail.com>
2022-09-21 20:55:54 -0300

    mudando a branch para acostumar

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 class About extends React.Component {
-    render() {
-      return (
-        <>
+  render() {
+    return (
+      <>
         <footer className="contato">
-        <h3> Contato</h3>
-        <ul className="nav-links">
-
-      <a href="https://www.linkedin.com/in/estevaomarqueso/">linkedin</a>
-      <a href="https://github.com/estevao144">GitHub</a>
-            
-        </ul>    
-            </footer>
-        </>
-      );
-    }
+          <h3> Contato</h3>
+          <ul className="nav-links">
+            <a href="https://github.com/estevao144">GitHub</a>
+            <li>
+              <Link to="https://www.linkedin.com/in/estevaomarqueso/">
+                {" "}
+                Linkedin{" "}
+              </Link>
+            </li>
+          </ul>
+        </footer>
+      </>
+    );
   }
-  
-  export default About;
+}
+
+export default About;
